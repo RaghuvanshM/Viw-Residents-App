e3f01ddbfa7a6d365896b748366513722748f238
Raghuvansh Mani <raghuvansh.mani@piri.ai>
2021-04-21 16:57:45 +0530

    “[Raghuvansh-developement]”

---
+++ src/screens/Startwindow.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+export const Startwindow: FC = () => {
+  return (
+    <View>
+      <Text>Screen 1</Text>
+    </View>
+  );
+};
+
+export default Startwindow
